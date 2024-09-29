b784d24fc4184910a385fab3eb2da789c80762e8
RoaaNasser20 <140814776+RoaaNasser20@users.noreply.github.com>
2024-09-29 22:27:51 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'pages/welcome_page.dart'; // Import WelcomePage
-import 'pages/quiz_page.dart'; // Import QuizPage
+import 'pages/welcome_page.dart'; 
+import 'pages/quiz_page.dart'; 
 
 void main() {
   runApp(QuizApp());
